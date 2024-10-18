cd7398b5ec4527a81179c71400ff2e76dab8270e
eliasjrdev <eliasjrcosta886@gmail.com>
2024-10-18 20:40:39 -0300

    menu drop-dow

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ function toggleMode(){
        html.classList.add('lightColor') 
     }
 }
+
+// Seleciona o botão e adiciona o evento de clique
+document.getElementById('dropdown-btn').addEventListener('click', function() {
+    this.parentElement.classList.toggle('show'); // Alterna a classe "show" no dropdown
+  });
+  
+  // Fecha o dropdown se clicar fora dele
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-btn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      for (var i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
+  
